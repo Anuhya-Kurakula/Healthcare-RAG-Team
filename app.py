cae659872796c2fbdb0070a2fad96b03d0f9ec0b
Pavani Dangudubiyyam <24b01a1227@svecw.edu.in>
2026-05-20 22:16:41 +0530

    fixed dependency conflicts for streamlit deployment

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from langchain_text_splitters import (
     RecursiveCharacterTextSplitter
 )
 
-from langchain_huggingface import (
+from langchain_community.embeddings import (
     HuggingFaceEmbeddings
 )
 
